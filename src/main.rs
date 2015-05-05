9b0b11101b36baffe45170dee984bcfc42d08498
Peter Anderson <anderson.peter@byu.edu>
2015-05-04 23:23:16 -0600

    Initial server setup to serve hello Pete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate iron;
+
+use iron::request::Request;
+use iron::response::Response;
+use iron::Iron;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+    Iron::new(|_: &mut Request| {
+        Ok(Response::with((status::Ok, "Hi, Pete")))
+    }).http("localhost:1337").unwrap();
 }
